0c8d140fa84a430c56ac93985e3b57d52654f227
Alan Oliveira <alan_victor_16@hotmail.com>
2024-07-23 10:58:57 -0300

    refactor: CarRegistration page form completed

--- resources/js/Pages/CarRegistration.tsx
+++ resources/js/Pages/CarRegistration.tsx
@@ -28,6 +28,14 @@ export default function CarRegistration({ auth }: PageProps) {
     plate: '',
   });
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value,
+    }));
+  };
+
   useEffect(() => {
     console.log('FormData updated:', formData);
   }, [formData]);
@@ -42,10 +50,118 @@ export default function CarRegistration({ auth }: PageProps) {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div>Form</div>
+            <form className="max-w-sm mx-auto">
+
+              <div className="mb-5">
+                <label htmlFor="manufacturer" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Manufacturer</label>
+                <input
+                  type="text"
+                  id="manufacturer"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Manufacturer"
+                  value={formData.manufacturer}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="model" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Model</label>
+                <input
+                  type="text"
+                  id="model"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Model"
+                  value={formData.model}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="exchange" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Exchange</label>
+                <input
+                  type="text"
+                  id="exchange"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Exchange"
+                  value={formData.exchange}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="version" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Version</label>
+                <input
+                  type="text"
+                  id="version"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Version"
+                  value={formData.version}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="fuel" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fuel</label>
+                <input
+                  type="text"
+                  id="fuel"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Fuel"
+                  value={formData.fuel}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="year" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
+                <input
+                  type="number"
+                  id="year"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Year"
+                  value={formData.year}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="dailyPrice" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Daily Price</label>
+                <input
+                  type="number"
+                  id="dailyPrice"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Daily Price"
+                  value={formData.dailyPrice}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="plate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Plate</label>
+                <input
+                  type="text"
+                  id="plate"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Plate"
+                  value={formData.plate}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+
           </div>
         </div>
       </div>
     </AuthenticatedLayout>
   );
-};
+}
