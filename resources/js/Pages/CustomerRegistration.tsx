68b57fa2619bde2870294b3c8ee40525357d6c9a
Alan Oliveira <alan_victor_16@hotmail.com>
2024-07-25 12:04:27 -0300

    feat: create CustomerRegistration page

---
+++ resources/js/Pages/CustomerRegistration.tsx
@@ -0,0 +1,203 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+import { PageProps } from '@/types';
+import axios from 'axios';
+//import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+// POST
+interface FormData {
+    full_name: string;
+    gender: string;
+    cpf: number;
+    phone: number;
+    country: string;
+    state: string;
+    city: string;
+    street_or_avenue: string | number | string & number;
+    numberOfAddress: number;
+}
+
+export default function CustomerRegistration({ auth }: PageProps) {
+    const [formData, setFormData] = useState<FormData>({
+        full_name: '',
+        gender: '',
+        cpf: 0,
+        phone: 0,
+        country: '',
+        state: '',
+        city: '',
+        street_or_avenue: '',
+        numberOfAddress: 0,
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [id]: value,
+        }));
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('/api/', formData);
+            console.log('Form submitted successfully:', response.data);
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+    };
+
+    useEffect(() => {
+        console.log('FormData updated:', formData);
+    }, [formData]);
+
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">CustomerRegistration</h2>}
+        >
+            <Head title="CustomerRegistration" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
+
+                            <div className="mb-5">
+                                <label htmlFor="full_name"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full name</label>
+                                <input
+                                    type="text"
+                                    id="full_name"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Full name"
+                                    value={formData.full_name}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="gender"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
+                                <input
+                                    type="text"
+                                    id="gender"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Gender"
+                                    value={formData.gender}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="cpf"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">CPF</label>
+                                <input
+                                    type="number"
+                                    id="cpf"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="CPF"
+                                    value={formData.cpf}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="phone"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone</label>
+                                <input
+                                    type="number"
+                                    id="phone"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Phone"
+                                    value={formData.phone}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="country"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
+                                <input
+                                    type="text"
+                                    id="country"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Country"
+                                    value={formData.country}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="state"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">State</label>
+                                <input
+                                    type="text"
+                                    id="state"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="State"
+                                    value={formData.state}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="city"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
+                                <input
+                                    type="text"
+                                    id="city"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="City"
+                                    value={formData.city}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="street_or_avenue"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Street or Avenue</label>
+                                <input
+                                    type="text"
+                                    id="street_or_avenue"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Street or Avenue"
+                                    value={formData.street_or_avenue}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <label htmlFor="numberOfAddress"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of Address</label>
+                                <input
+                                    type="number"
+                                    id="numberOfAddress"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="No."
+                                    value={formData.street_or_avenue}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
+                            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
