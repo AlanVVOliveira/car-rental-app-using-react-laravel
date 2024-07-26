a2fc87ef73ee2179f04b4ea2876a377097b1eb1c
Alan Oliveira <alan_victor_16@hotmail.com>
2024-07-26 14:49:54 -0300

    feat: create clients_table

---
+++ database/migrations/2024_07_26_174603_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('gender');
+            $table->integer('cpf');
+            $table->integer('phone');
+            $table->string('country');
+            $table->string('state');
+            $table->string('city');
+            $table->string('street_or_avenue');
+            $table->integer('numberOfAddress');
+            $table->boolean('isActive')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
