f9070a8fae299ff9ae155855ca95600b9647abf2
Alan Oliveira <alan_victor_16@hotmail.com>
2024-08-16 13:24:39 -0300

    feat: add validation in ClientController and refactor validation CarController

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -28,8 +28,9 @@ class CarController extends Controller
                 'version' => 'required|regex:/^[\pL\s\d]+$/u|min:2|max:40',
                 'fuel' => 'required|regex:/^[\pL\s\d]+$/u|min:2|max:40',
                 'year' => 'required|regex:/^[\d]{4}$/',
-                'dailyPrice' => 'required|numeric|max:10',
-                'plate' => 'required|regex:/^[A-Z]{3}-?[A-J0-9]\d{4}$/'
+                'dailyPrice' => 'required|numeric|max:9999999999',
+                //'plate' => 'required|regex:/^(?:[A-Za-z]{3}\d{4}|[A-Za-z]{3}\d[A-Za-z]\d{2})$/'
+                'plate' => 'required|regex:/^[A-Za-z]{3}\d{4}$/'
             ];
             
             $messages = [
@@ -37,34 +38,27 @@ class CarController extends Controller
                 'manufacturer.regex' => 'The manufacturers name may contain letters, numbers and spaces.',
                 'manufacturer.max' => 'The manufacturers name must be a maximum of 40 characters.',
                 'manufacturer.min' => 'The manufacturers name must have at least 2 characters.',
-
                 'model.required' => 'Model name is required.',
                 'model.regex' => 'The model name may contain letters, numbers and spaces.',
                 'model.max' => 'The model name must be a maximum of 40 characters.',
                 'model.min' => 'The model name must have at least 2 characters.',
-
                 'exchange.required' => 'Exchange is required.',
                 'exchange.regex' => 'The exchange reference may contain letters, numbers and spaces.',
                 'exchange.max' => 'The exchange reference must be a maximum of 40 characters.',
                 'exchange.min' => 'The exchange reference must have at least 2 characters.',
-
                 'version.required' => 'Version is required.',
                 'version.regex' => 'The version reference may contain letters, numbers and spaces.',
                 'version.max' => 'The version reference must be a maximum of 40 characters.',
                 'version.min' => 'The version reference must have at least 2 characters.',
-
                 'fuel.required' => 'Fuel is required.',
                 'fuel.regex' => 'The fuel reference may contain letters, numbers and spaces.',
                 'fuel.max' => 'The fuel reference must be a maximum of 40 characters.',
                 'fuel.min' => 'The fuel reference must have at least 2 characters.',
-
                 'year.required' => 'Year is required.',
                 'year.regex' => 'The year reference must contain 4 numbers.',
-
                 'dailyPrice.required' => 'The daily price is required.',
                 'dailyPrice.numeric' => 'The daily price must be a numeric value.',
                 'dailyPrice.max' => 'The daily price must have a maximum of 10 characters.',
-
                 'plate.required' => 'The plate is required.',
                 'plate.regex' => 'Accepts the old standard or the new Mercosur format. (AAA3333 or DVA-3F33).'
             ];
--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,18 +6,13 @@ use App\Models\Client;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class ClientController extends Controller
 {
     public function index(Request $request)
     {
-        // Provisional Data
         $clients = Client::where('isActive', 1)->get();
-        /*$clients = array(
-            array("id" => 1, "name" => "John Travolta"),
-            array("id" => 2, "name" => "Will Smith"),
-            array("id" => 3, "name" => "Bill Gates"),
-        );*/
 
         return response()->json($clients);
     }
@@ -25,18 +20,82 @@ class ClientController extends Controller
     public function store(Request $request)
     {
         try {
-            Client::create([
-                'full_name'  => $request->input('full_name'),
-                'gender' => $request->input('gender'), 
-                'cpf' => $request->input('cpf'),
-                'phone' => $request->input('phone'),
-                'country' => $request->input('country'),
-                'state' => $request->input('state'),
-                'city' => $request->input('city'),
-                'street_or_avenue' => $request->input('street_or_avenue'),
-                'number_of_address' => $request->input('number_of_address'),
-            ]);
-            return response()->json(['message' => 'Successfully!']);
+            $rules = [
+                'full_name' => 'required|regex:/^[\pL\s]+$/u|min:2|max:255',
+                'gender'=> 'required|regex:/^[\pL\s]+$/u|min:2|max:40',
+                'cpf' => 'required|regex:/^\d{11}$/',
+                'phone' => 'required|regex:/^\d{11}$/',
+                'country' => 'required|regex:/^[\pL\s]+$/u|min:2|max:255',
+                'state' => 'required|regex:/^[\pL\s]+$/u|min:2|max:255',
+                'city' => 'required|regex:/^[\pL\s]+$/u|min:2|max:255',
+                'street_or_avenue' => 'required|regex:/^[\pL\s]+$/u|min:2|max:9999999999',
+                'number_of_address' => 'required|numeric|max:9999999999',
+            ];
+            
+            $messages = [
+                'full_name.required' => 'Full name is required.',
+                'full_name.regex' => 'Full name may contain letters and spaces.',
+                'full_name.max' => 'Full name must be a maximum of 255 characters.',
+                'full_name.min' => 'Full name must have at least 2 characters.',
+
+                'gender.required' => 'The gender is required.',
+                'gender.regex' => 'The gender may contain letters and spaces.',
+                'gender.max' => 'The gender must be a maximum of 40 characters.',
+                'gender.min' => 'The gender must have at least 2 characters.',
+
+                'cpf.required' => 'CPF is required.',
+                'cpf.regex' => 'Enter the cpf without special characters.',
+
+                'phone.required' => 'Telephone number is required.',
+                'phone.regex' => 'Enter telephone number without special characters.',
+
+                'country.required' => 'Country name is required.',
+                'country.regex' => 'country name may contain letters and spaces.',
+                'country.max' => 'Country name must be a maximum of 255 characters.',
+                'country.min' => 'Country name must have at least 2 characters.',
+
+                'state.required' => 'State name is required.',
+                'state.regex' => 'State name may contain letters and spaces.',
+                'state.max' => 'State name must be a maximum of 255 characters.',
+                'state.min' => 'State name must have at least 2 characters.',
+
+                'city.required' => 'City name is required.',
+                'city.regex' => 'City name may contain letters and spaces.',
+                'city.max' => 'City name must be a maximum of 255 characters.',
+                'city.min' => 'City name must have at least 2 characters.',
+
+                'street_or_avenue.required' => 'Street or avenue is required.',
+                'street_or_avenue.regex' => 'Street or avenue may contain letters and spaces.',
+                'street_or_avenue.max' => 'Street or avenue must be a maximum of 255 characters.',
+                'street_or_avenue.min' => 'Street or avenue must have at least 2 characters.',
+                
+                'number_of_address.required' => 'The number of address is required.',
+                'number_of_address.numeric' => 'The number of address must be a numeric value.',
+                'number_of_address.max' => 'The number of address must have a maximum of 10 characters.'
+            ];
+
+            $validator = Validator::make($request->all(), $rules, $messages);
+
+            if ($validator->fails()) {
+
+                return response()->json(['errors' => $validator->errors()]);
+
+            } else {
+
+                Client::create([
+                    'full_name'  => $request->input('full_name'),
+                    'gender' => $request->input('gender'), 
+                    'cpf' => $request->input('cpf'),
+                    'phone' => $request->input('phone'),
+                    'country' => $request->input('country'),
+                    'state' => $request->input('state'),
+                    'city' => $request->input('city'),
+                    'street_or_avenue' => $request->input('street_or_avenue'),
+                    'number_of_address' => $request->input('number_of_address'),
+                ]);
+
+                return response()->json(['message' => 'Successfully!']);
+            }
         } catch (Exception $e) {
             Log::error($e->getMessage());
             Log::info($request->all());
