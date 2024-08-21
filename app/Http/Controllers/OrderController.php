0f0a27205dbbee8686c7d61444e7973a3184788c
Alan Oliveira <alan_victor_16@hotmail.com>
2024-08-21 10:07:03 -0300

    feat: add car availability check before rental confirmation

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -29,21 +29,24 @@ class OrderController extends Controller
     public function store(Request $request)
     {
         try {
-            $order = Order::create([
-                'client_id' => $request->input('client_id'),
-                'car_id' => $request->input('car_id'),
-                'rental_start_date' => Carbon::parse($request->input('rental_start_date'))->format('Y-m-d H:i:s'),
-                'rental_end_date' => Carbon::parse($request->input('rental_end_date'))->format('Y-m-d H:i:s'),
-                'number_of_rent_days' => $request->input('number_of_rent_days'),
-                'total' => $request->input('total'),
-            ]);
-
             // leave the vehicle unavailable during the rental period
             $car = Car::findOrFail($request->input('car_id'));
-            $car->isAvailable = 0;
-            $car->save();
-
-            return response()->json(['message' => 'Successfully!']);
+            if ($car->isAvailable == 1) {
+                $order = Order::create([
+                    'client_id' => $request->input('client_id'),
+                    'car_id' => $request->input('car_id'),
+                    'rental_start_date' => Carbon::parse($request->input('rental_start_date'))->format('Y-m-d H:i:s'),
+                    'rental_end_date' => Carbon::parse($request->input('rental_end_date'))->format('Y-m-d H:i:s'),
+                    'number_of_rent_days' => $request->input('number_of_rent_days'),
+                    'total' => $request->input('total'),
+                ]);
+                
+                $car->isAvailable = 0;
+                $car->save();
+                return response()->json(['message' => 'Successfully!']);
+            } else {
+                return response()->json(['message' => 'The car is now unavailable!']);
+            }
         } catch (Exception $e) {
             Log::error($e->getMessage());
             Log::info($request->all());
